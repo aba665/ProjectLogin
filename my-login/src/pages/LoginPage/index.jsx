975d461164abd9a020f708f77ffaa2b0063177e8
aba665 <abissons@gmail.com>
2022-02-05 20:50:54 -0300

    all right

---
+++ my-login/src/pages/LoginPage/index.jsx
@@ -0,0 +1,45 @@
+import React,  { useState }  from 'react';
+import './style.css'
+
+const LoginPage = () => {
+
+    const [ email, setEmail ] = useState();
+    const [ password, setPassword ] = useState();
+
+    const handleSubmit = (e) => {
+          
+          e.preventDefault()
+
+          if(email.length <= 0  || password.length <= 0){
+
+              alert('Preencha todos os dados corretamente!');
+
+          }else{
+
+              console.log('Submit', email, password);   
+
+          }
+    }
+
+    return (
+        <div id='login'>
+          
+            <form className='form' onSubmit={handleSubmit}>
+            <h1 className='title'>Login Do Sistema</h1>
+                <div className='field'>
+                    <label htmlFor='email'>Email:</label>
+                    <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div className='field'>
+                    <label htmlFor='password'>Senha:</label>
+                    <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+              </div>
+              <div className='actions'>
+                  <button type='submit'>Entrar</button>
+              </div>
+            </form>
+        </div>
+    )
+}
+
+export default LoginPage;
